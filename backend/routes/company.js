4d29097d8fec0e379379290d34db90b32b5a268c
qiuyixianq <qiuyixianq@gmail.com>
2021-06-19 21:05:41 +0800

    finalize company schema for now and keyed in 10 sample data in mongo

--- backend/routes/company.js
+++ backend/routes/company.js
@@ -4,15 +4,23 @@ const router = express.Router();
 //Models
 const Company = require('../Models/company_model');
 
+
+//API
+router.get('/', (req,res) => {
+    Company.find((err,docs) => err? res.send(err) : res.send(docs));
+})
+
 router.post('/', (req,res) => {
-    const { name, size, type, industry, location, job } = req.body;
+    const { name, website, size, type, industry, location, job, imgsrc } = req.body;
     const newCompany = new Company({
         companyName: name,
+        companyWebsite: website,
         companySize: size,
         companyType: type,
         companyIndustry: industry,
         companyLocation: location,
-        companyJob: job
+        companyJob: job,
+        logo: imgsrc
     });
 
     newCompany.save(err => err ? res.send(err) : res.send('company saved'));
