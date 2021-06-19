f5fdb86ee50a20fa84143cf7fd476da348c65068
qiuyixianq <qiuyixianq@gmail.com>
2021-06-19 17:29:28 +0800

    working post (Create) for company

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,32 @@
-const express = require('express');
+const express = require('express'); 
 const app = express();
 require('dotenv').config();
+const mongoose = require('mongoose');
 
-app.listen(process.env.PORT, () => console.log(`Backend is listening on ${process.env.PORT}`));
+//Models
+const Company = require('./Models/company_model');
 
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//Port
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Backend is listening on ${PORT}`));
+
+//MongoDB
+mongoose.connect(process.env.MONGO_CONNECTION,{useNewUrlParser: true, useUnifiedTopology:true}, () => 'Connected to MongoDB');
+
+//REST
 app.get('/', (req,res) => {
-    res.send('Hello World')
+    res.send('Hello World');
+});
+
+app.post('/',(req,res) => {
+    const { name, type } = req.body;
+    const newCompany = new Company({
+        companyName: name, companyType: type
+    });
+
+    newCompany.save(err => err ? res.send(err) : res.send('company saved'));
 })
