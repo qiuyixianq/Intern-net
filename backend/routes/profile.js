e19a96f3bb2456efe3f653f510396c766a39f85d
qiuyixianq <qiuyixianq@gmail.com>
2021-06-29 18:22:08 +0800

    Refactored & tested appliedJob backend

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const User = require('../Models/user_model');
 //API
 //PUT -> save company
-router.put('/', async (req,res) => {
+router.put('/', async (req, res) => {
     const { username, companyName, jobTitle } = req.body;
 
-    try{
+    try {
         //const result = await User.findOne({username});
         //const userResult = result._doc;
 
@@ -18,35 +18,58 @@ router.put('/', async (req,res) => {
             appliedJob: [{
                 company: 'iFAST',
                 job: ['software intern']
-            }]
+            },
+            {
+                company: 'iFAST2',
+                job: ['something intern']
+            }
+        ]
         };
 
         //user found
-        if(Object.keys(userResult).length !== 0){
+        if (Object.keys(userResult).length !== 0) {
             //check if existing saved job in same company
-            const savedCompany = userResult.appliedJob.find(save => save.company === companyName);
-            const savedCompanyIndex = userResult.appliedJob.findIndex( save => save.company === companyName);
+            const savedCompany = userResult.appliedJob.find(saved => saved.company === companyName);
+            const savedCompanyIndex = userResult.appliedJob.findIndex(saved => saved.company === companyName);
 
             //exist
-            if(savedCompany){
-                //clone appliedJob
-                let updatedAppliedJob = [...userResult.appliedJob]
-                //update it
-                updatedAppliedJob[savedCompanyIndex] = {...savedCompany, job: [...savedCompany.job, jobTitle]};
-                //clone user and update new appliedJob
-                const updatedUser = {...userResult, appliedJob: updatedAppliedJob}
-                res.json(updatedUser);
+            if (savedCompany) {
+                //job exist ? remove : add
+
+                //clone appliedJob array
+                let updatedAppliedJob = [...userResult.appliedJob];
+
+                //remove
+                if (savedCompany.job.find( job => job === jobTitle)) {
+                    //filter out saved job
+                    let updatedJob = savedCompany.job.filter(job => job !== jobTitle);
+
+                    //reassign into appliedJob array
+                    updatedAppliedJob[savedCompanyIndex] = {...savedCompany, job: updatedJob};
+
+                    //reassign into user
+                    const updatedUser = {...userResult, appliedJob: updatedAppliedJob};
+                    res.json(updatedUser);
+                }
+                //append
+                else {
+                    //append new job in 'job' array
+                    updatedAppliedJob[savedCompanyIndex] = { ...savedCompany, job: [...savedCompany.job, jobTitle] };
+                    //reassign into user
+                    const updatedUser = { ...userResult, appliedJob: updatedAppliedJob }
+                    res.json(updatedUser);
+                }
             }
             //new save obj
             else {
                 //clone appliedJob
-                let updateAppliedJob = [...userResult.appliedJob, {company: companyName, job:[jobTitle]}];
-                const updatedUser = {...userResult, appliedJob: updateAppliedJob};
+                let updateAppliedJob = [...userResult.appliedJob, { company: companyName, job: [jobTitle] }];
+                const updatedUser = { ...userResult, appliedJob: updateAppliedJob };
                 res.json(updatedUser);
             }
         }
     }
-    catch(err) { console.log(err) }
+    catch (err) { console.log(err) }
 
 });
 
