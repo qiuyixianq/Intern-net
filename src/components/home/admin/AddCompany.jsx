2fc15a4a329df3d272e7af91ea9190b3501a46af
qiuyixianq <qiuyixianq@gmail.com>
2021-07-28 19:53:52 +0800

    Implementation UI Admin.CompanyRegistration
    apply basic styling to company registration form

--- src/components/home/admin/AddCompany.jsx
+++ src/components/home/admin/AddCompany.jsx
@@ -22,16 +22,16 @@ const initialJob = {
 
 export const AddCompany = () => {
     const [company, setCompany] = useState(initialCompany);
-    const [jobArr, setJobArr] = useState([{...initialJob}, {...initialJob}]);
+    const [jobArr, setJobArr] = useState([{ ...initialJob }, { ...initialJob }]);
 
-    const addJob = () => setJobArr([...jobArr,initialJob]);
+    const addJob = () => setJobArr([...jobArr, initialJob]);
 
-    const handleRemoveJob = index => setJobArr(jobArr.filter((job,arrIndex) => arrIndex !== index));
+    const handleRemoveJob = index => setJobArr(jobArr.filter((job, arrIndex) => arrIndex !== index));
 
     //Company Basic info changes
     const handleChange = el => {
         const { id, value } = el.target;
-        setCompany({...company, [id]: value});
+        setCompany({ ...company, [id]: value });
     }
 
     //JobInput.jsx changes
@@ -44,53 +44,59 @@ export const AddCompany = () => {
 
     const handleSubmit = () => {
         //compile request object
-        const reqObj = {...company};
+        const reqObj = { ...company };
         reqObj.job = jobArr;
 
         axios.post('http://localhost:4000/company', reqObj);
     }
 
+
+    //main render
     return (
         <center>
-            <form onSubmit={() => handleSubmit()} style={{ width: '50%', textAlign: 'left' }} className="addCompanyForm">
+            <form onSubmit={() => handleSubmit()} className="addCompanyForm">
 
-                <div className="mb-5">
+                <div className="mb-2">
                     <h5>Company Registration Form</h5>
-                    <label htmlFor="name">Name</label>
-                    <input type="text" className="form-control mb-3" id="name" placeholder="Company Name" onChange={ el => handleChange(el)} required />
+                    <div className="basicInformation">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" className="form-control mb-3" id="name" placeholder="Company Name" onChange={el => handleChange(el)} required />
 
-                    <label htmlFor="website">Website</label>
-                    <input type="text" className="form-control mb-3" id="website" placeholder="Company Website" onChange={ el => handleChange(el)} required />
+                        <label htmlFor="website">Website</label>
+                        <input type="text" className="form-control mb-3" id="website" placeholder="Company Website" onChange={el => handleChange(el)} required />
 
-                    <p>Company Size</p>
-                    <div className="d-flex justify-content-around mb-3">
-                        <div>
-                            <label htmlFor="sizeFrom" className="me-2">From</label>
-                            <input type="number" id="sizeFrom" onChange={ el => handleChange(el)} required />
+                        <p>Company Size</p>
+                        <div className="d-flex justify-content-around mb-3">
+                            <div>
+                                <label htmlFor="sizeFrom" className="me-2">From</label>
+                                <input type="number" id="sizeFrom" onChange={el => handleChange(el)} required />
+                            </div>
+
+                            <div>
+                                <label htmlFor="sizeTo" className="me-2">To</label>
+                                <input type="number" id="sizeTo" onChange={el => handleChange(el)} required />
+                            </div>
                         </div>
 
-                        <div>
-                            <label htmlFor="sizeTo" className="me-2">To</label>
-                            <input type="number" id="sizeTo" onChange={ el => handleChange(el)} required />
-                        </div>
+                        <label htmlFor="type">Type</label>
+                        <input type="text" className="form-control mb-3" id="type" placeholder="Company Type" onChange={el => handleChange(el)} required />
+
+                        <label htmlFor="industry">Industry</label>
+                        <input type="text" className="form-control mb-3" id="industry" placeholder="Company Industry" onChange={el => handleChange(el)} required />
+
+                        <label htmlFor="location">Location</label>
+                        <input type="text" className="form-control mb-3" id="location" placeholder="Company Location" onChange={el => handleChange(el)} required />
+
+                        <label htmlFor="imgsrc">Logo link</label>
+                        <input type="text" className="form-control mb-3" id="imgsrc" placeholder="Company Logo URL" onChange={el => handleChange(el)} />
                     </div>
 
-                    <label htmlFor="type">Type</label>
-                    <input type="text" className="form-control mb-3" id="type" placeholder="Company Type" onChange={ el => handleChange(el)} required />
-
-                    <label htmlFor="industry">Industry</label>
-                    <input type="text" className="form-control mb-3" id="industry" placeholder="Company Industry" onChange={ el => handleChange(el)} required />
-
-                    <label htmlFor="location">Location</label>
-                    <input type="text" className="form-control mb-3" id="location" placeholder="Company Location" onChange={ el => handleChange(el)} required />
-
-                    <div>
+                    <div className="jobSection">
                         <p>Job <span role="button" onClick={() => addJob()}>add</span></p>
-                        {jobArr.map((job, index) => <JobInput index={index} key={index} handleRemoveJob={() => handleRemoveJob(index)} onChange={ el => handleJobChange(el)} />)}
+                        {jobArr.map((job, index) => <JobInput index={index} key={index} handleRemoveJob={() => handleRemoveJob(index)} onChange={el => handleJobChange(el)} />)}
                     </div>
 
-                    <label htmlFor="imgsrc">Logo link</label>
-                    <input type="text" className="form-control mb-3" id="imgsrc" placeholder="Company Logo URL" onChange={ el => handleChange(el)} />
+
 
                     <input type="submit" className="btn btn-info" />
                 </div>
