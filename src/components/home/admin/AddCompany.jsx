6343caa225bc1528d6b60d7d8fc2f003e88c2709
qiuyixianq <qiuyixianq@gmail.com>
2021-07-28 19:44:03 +0800

    Implementation FE BE -Admin.CompanyRegistration
    Working Frontend + Backend for addCompany

--- src/components/home/admin/AddCompany.jsx
+++ src/components/home/admin/AddCompany.jsx
@@ -6,7 +6,8 @@ const axios = require('axios').default;
 const initialCompany = {
     name: '',
     website: '',
-    size: '',
+    sizeFrom: '',
+    sizeTo: '',
     type: '',
     industry: '',
     location: '',
@@ -21,15 +22,32 @@ const initialJob = {
 
 export const AddCompany = () => {
     const [company, setCompany] = useState(initialCompany);
-    const [jobArr, setJobArr] = useState([initialJob, initialJob]);
+    const [jobArr, setJobArr] = useState([{...initialJob}, {...initialJob}]);
 
     const addJob = () => setJobArr([...jobArr,initialJob]);
 
     const handleRemoveJob = index => setJobArr(jobArr.filter((job,arrIndex) => arrIndex !== index));
 
+    //Company Basic info changes
+    const handleChange = el => {
+        const { id, value } = el.target;
+        setCompany({...company, [id]: value});
+    }
+
+    //JobInput.jsx changes
+    const handleJobChange = el => {
+        const { dataset } = el.target;
+        const tempJobArr = [...jobArr];
+        tempJobArr[dataset.index][dataset.field] = el.target.value;
+        setJobArr(tempJobArr);
+    }
+
     const handleSubmit = () => {
         //compile request object
-        axios.post('http://localhost:4000/company', company);
+        const reqObj = {...company};
+        reqObj.job = jobArr;
+
+        axios.post('http://localhost:4000/company', reqObj);
     }
 
     return (
@@ -39,39 +57,42 @@ export const AddCompany = () => {
                 <div className="mb-5">
                     <h5>Company Registration Form</h5>
                     <label htmlFor="name">Name</label>
-                    <input type="text" className="form-control mb-3" id="name" placeholder="Company Name" required />
+                    <input type="text" className="form-control mb-3" id="name" placeholder="Company Name" onChange={ el => handleChange(el)} required />
+
                     <label htmlFor="website">Website</label>
-                    <input type="text" className="form-control mb-3" id="website" placeholder="Company Website" required />
+                    <input type="text" className="form-control mb-3" id="website" placeholder="Company Website" onChange={ el => handleChange(el)} required />
 
                     <p>Company Size</p>
                     <div className="d-flex justify-content-around mb-3">
                         <div>
                             <label htmlFor="sizeFrom" className="me-2">From</label>
-                            <input type="number" id="sizeFrom" required />
+                            <input type="number" id="sizeFrom" onChange={ el => handleChange(el)} required />
                         </div>
 
                         <div>
                             <label htmlFor="sizeTo" className="me-2">To</label>
-                            <input type="number" id="sizeTo" required />
+                            <input type="number" id="sizeTo" onChange={ el => handleChange(el)} required />
                         </div>
                     </div>
 
                     <label htmlFor="type">Type</label>
-                    <input type="text" className="form-control mb-3" id="type" placeholder="Company Type" required />
+                    <input type="text" className="form-control mb-3" id="type" placeholder="Company Type" onChange={ el => handleChange(el)} required />
 
                     <label htmlFor="industry">Industry</label>
-                    <input type="text" className="form-control mb-3" id="industry" placeholder="Company Industry" required />
+                    <input type="text" className="form-control mb-3" id="industry" placeholder="Company Industry" onChange={ el => handleChange(el)} required />
 
                     <label htmlFor="location">Location</label>
-                    <input type="text" className="form-control mb-3" id="location" placeholder="Company Location" required />
+                    <input type="text" className="form-control mb-3" id="location" placeholder="Company Location" onChange={ el => handleChange(el)} required />
 
                     <div>
                         <p>Job <span role="button" onClick={() => addJob()}>add</span></p>
-                        {jobArr.map((job, index) => <JobInput index={index} key={index} handleRemoveJob={() => handleRemoveJob(index)} />)}
+                        {jobArr.map((job, index) => <JobInput index={index} key={index} handleRemoveJob={() => handleRemoveJob(index)} onChange={ el => handleJobChange(el)} />)}
                     </div>
 
                     <label htmlFor="imgsrc">Logo link</label>
-                    <input type="text" className="form-control mb-3" id="imgsrc" placeholder="Company Logo URL" />
+                    <input type="text" className="form-control mb-3" id="imgsrc" placeholder="Company Logo URL" onChange={ el => handleChange(el)} />
+
+                    <input type="submit" className="btn btn-info" />
                 </div>
 
             </form>
--- src/components/home/admin/JobInput.jsx
+++ src/components/home/admin/JobInput.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 export const JobInput = props => {
-    const { index, handleRemoveJob } = props;
+    const { index, handleRemoveJob, onChange } = props;
 
     return (
         <div key={index} >
@@ -11,10 +11,10 @@ export const JobInput = props => {
                 <span className="ms-2" role="button" onClick={ () => handleRemoveJob(index)}>remove</span>
             </p>
             <label htmlFor={`job${index}Title`}>Job Title</label>
-            <input type="text" className="form-control mb-3" id={`job${index}Title`} />
+            <input type="text" className="form-control mb-3" id={`job${index}Title`} data-index={index} data-field='title' onChange={el => onChange(el)} required />
 
             <label htmlFor={`job${index}Desc`}>Job Description</label>
-            <input type="text" className="form-control mb-3" id={`job${index}Desc`} />
+            <input type="text" className="form-control mb-3" id={`job${index}Desc`} data-index={index} data-field='description' onChange={el => onChange(el)} required />
         </div>
     )
 }
