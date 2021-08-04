dd63798388b2f08789b9ac3847c531732983d8af
qiuyixianq <qiuyixianq@gmail.com>
2021-08-04 14:33:43 +0800

    Bug Fix FE BE -CompanyDetail savedJob
    Invalid /username/ -> /email/ frontend backend body.req field

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -6,12 +6,12 @@ const User = require('../Models/user_model');
 //API
 //PUT -> save company
 router.put('/', async (req, res) => {
-    const { username, companyName, jobTitle } = req.body;
+    const { email, companyName, jobTitle } = req.body;
 
     try {
-        const result = await User.findOne({username});
+        const result = await User.findOne({email});
         const userResult = result._doc;
-
+        
         //user found
         if (Object.keys(userResult).length !== 0) {
             //check if existing saved job in same company
@@ -27,7 +27,6 @@ router.put('/', async (req, res) => {
 
                 //remove
                 if (savedCompany.job.find( job => job === jobTitle)) {
-                    console.log('exist & remove');
                     //filter out saved job
                     let updatedJob = savedCompany.job.filter(job => job !== jobTitle);
 
@@ -35,30 +34,28 @@ router.put('/', async (req, res) => {
                     //**IMPORANT {...savedCompany} !== savedCompany //Must refer to _doc keyword
                     updatedSavedJob[savedCompanyIndex] = {...savedCompany._doc, job: updatedJob};
 
-                    User.findOneAndUpdate({username},{savedJob: updatedSavedJob},{new: true},(err,doc) => {
+                    User.findOneAndUpdate({email},{savedJob: updatedSavedJob},{new: true},(err,doc) => {
                         err? console.log(err) : res.json(doc);
                     });
                 }
                 //append
                 else {
-                    console.log('exist & append');
                     //append new job in 'job' array
                     //**IMPORANT {...savedCompany} !== savedCompany //Must refer to _doc keyword
                     updatedSavedJob[savedCompanyIndex] = { ...savedCompany._doc, job: [...savedCompany.job, jobTitle] };
 
-                    User.findOneAndUpdate({username},{ savedJob: updatedSavedJob},{new: true},(err,docs) => {
+                    User.findOneAndUpdate({email},{ savedJob: updatedSavedJob},{new: true},(err,docs) => {
                         err? console.log(err) : res.json(docs);
                     });
                 }
             }
             //new save obj
             else {
-                console.log('!exist & add');
                 //clone savedJob
                 let updateAppliedJob = [...userResult.savedJob, { company: companyName, job: [jobTitle] }];
                 const updatedUser = { ...userResult, savedJob: updateAppliedJob };
                 
-                User.findOneAndUpdate({username},updatedUser,{new: true},(err,docs) => {
+                User.findOneAndUpdate({email},updatedUser,{new: true},(err,docs) => {
                     err? console.log(err) : res.json(docs);
                 });
             }
