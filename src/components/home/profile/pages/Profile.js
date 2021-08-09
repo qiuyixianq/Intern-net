b6382fb12bc6d85db6ee71f5a8c67f6406bfc068
huajiann <huajianchan0206@gmail.com>
2021-08-09 21:03:16 +0800

    update profile ui

--- src/components/home/profile/pages/Profile.js
+++ src/components/home/profile/pages/Profile.js
@@ -1,116 +1,257 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import ProfImg from "../assets/images/prof_test.jpeg";
+import ProfImg from "../assets/images/prof_test_crop.jpeg";
 import "./ProfileTemp.css";
+import { Chart, Doughnut } from "react-chartjs-2";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
-  const { user } = useSelector(state => state.authentication);
-  
+  const { user } = useSelector((state) => state.authentication);
+
+  const data = {
+    legend: {
+      display: false,
+    },
+    datasets: [
+      {
+        label: "Javascript",
+        data: [9, 1],
+        backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(255, 255, 255, 1)"],
+      },
+    ],
+  };
+
+  const data2 = {
+    legend: {
+      display: false,
+    },
+    datasets: [
+      {
+        label: "Javascript",
+        data: [6, 4],
+        backgroundColor: ["rgba(75, 192, 192, 0.2)", "rgba(255, 255, 255, 1)"],
+      },
+    ],
+  };
+
+  const data3 = {
+    legend: {
+      display: false,
+    },
+    datasets: [
+      {
+        label: "Javascript",
+        data: [7, 3],
+        backgroundColor: ["rgba(54, 162, 235, 0.2)", "rgba(255, 255, 255, 1)"],
+      },
+    ],
+  };
+
   return (
     <>
       <div className="container">
         <div className="main-body">
-          <div className="row gutters-sm">
-            <div className="col-md-3 mb-3">
-              <div className="card card-rounded">
-                <div className="profile_image">
-                  <img src={ProfImg} alt="" />
+          <div className="row profile_main">
+            <h1>Profile Information</h1>
+            <div className="card card-rounded animated animatedFadeInUp fadeInUp">
+              <div className="row row-no-gutters profile_overview">
+                <div className="col-sm-4 ">
+                  <div className="profile_image">
+                    <img src={ProfImg} alt="" />
+                  </div>
+                  <pre />
+                  <Link to="/profile/overview/profileinfo">
+                    <button className="button">Edit Profile</button>
+                  </Link>
                 </div>
-                <div className="profile_name">
-                  <div className="name">{`${user.firstName}, ${user.surName}`}</div>
-                  <div className="title">{user.profession}</div>
+                <div className="col-sm-4 profile_right border-end">
+                  <div className="profile_name">
+                    <div className="name">{`${user.firstName}, ${user.surName}`}</div>
+                    <div className="title">{user.profession}</div>
+                  </div>
+                  <div className="profile_info">
+                    <ul className="profile_timeline">
+                      <li className="d-flex align-items-center flex-wrap">
+                        <i className="bx bxs-phone prof_icons"></i>
+                        <div>{user.mobile}</div>
+                      </li>
+                      <li className="d-flex align-items-center flex-wrap">
+                        <i className="bx bx-envelope prof_icons"></i>
+                        <div>{user.email}</div>
+                      </li>
+                      <li className="d-flex align-items-center flex-wrap">
+                        <i class="bx bxs-map prof_icons"></i>
+                        <div>Kuala Lumpur, Malaysia</div>
+                      </li>
+                      <li className="d-flex align-items-center flex-wrap">
+                        <i className="bx bxl-linkedin-square prof_icons"></i>
+                        <a
+                          href="https://www.linkedin.com"
+                          target="_blank"
+                          rel="noreferrer"
+                          style={{
+                            textDecoration: "none",
+                            color: "#000",
+                            fontWeight: "600",
+                          }}
+                        >
+                          {user.firstName}
+                        </a>
+                        <i
+                          class="bx bx-link-external"
+                          style={{ fontSize: "12px" }}
+                        ></i>
+                      </li>
+                      <li className="d-flex align-items-center flex-wrap">
+                        <i className="bx bx-paperclip prof_icons"></i>
+                        <a
+                          href="https://www.uowmkdu.edu.my/"
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          Resume Link
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
-                <div className="profile_info">
-                  <ul className="profile_timeline">
-                    <li className="d-flex align-items-center flex-wrap">
-                      <i className="bx bxs-phone prof_icons"></i>
-                      <div>{user.mobile}</div>
-                    </li>
-                    <li className="d-flex align-items-center flex-wrap">
-                      <i className="bx bx-envelope prof_icons"></i>
-                      <div>{user.email}</div>
-                    </li>
-                    <li className="d-flex align-items-center flex-wrap">
-                      <i className="bx bxl-linkedin-square prof_icons"></i>
-                      <div>{user.firstName}</div>
-                    </li>
-                  </ul>
-                  <br />
-                  <button>Contact Me</button>
+                <div className="col-sm-2 ms-md-auto">
+                  <p>Skills</p>
+                  <Doughnut
+                    options={{ tooltips: { enabled: false } }}
+                    data={data}
+                  />
+                  <pre></pre>
+                  <p>Language</p>
+                  <Doughnut
+                    options={{ tooltips: { enabled: false } }}
+                    data={data3}
+                  />
                 </div>
               </div>
             </div>
+          </div>
+          <hr />
+          <h1>Education</h1>
+          <div className="row profile_main">
+            <div className="card card-rounded animated animatedFadeInUp fadeInUp lazy">
+              <div className="main-timeline">
+                <div className="timeline">
+                  <div className="icon"></div>
+                  <div className="date-content">
+                    <div className="date-outer">
+                      <span className="date">
+                        <span className="year">2022</span>
+                        <span className="cgpa">CGPA 3.93</span>
+                      </span>
+                    </div>
+                  </div>
+                  <div className="timeline-content">
+                    <h5 className="title">UOW Malaysia KDU</h5>
+                    <p className="description">
+                      Bachelor in Software Engineering
+                    </p>
+                  </div>
+                </div>
 
-            <div className="col-md-8">
-              <div className="card mb-3 lazy">
-                <div className="card-body">
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Full Name</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">
-                      {`${user.surName} ${user.firstName}`}
-                    </div>
-                  </div>
-                  <hr />
-                  <div className="row">
-                    <h6 className="mb-0">Education</h6>
-                    <label className="col-sm-3 date-label text-secondary">
-                      March 2022
-                    </label>
-                    <div className="col-sm-9 ">UOW Malaysia KDU</div>
-                    <div className="col-sm-3"></div>
-                    <div className="col-sm-9 text-secondary">
-                      Bachelor Degree of Sofware Engineering (2022)
+                <div className="timeline">
+                  <div className="icon"></div>
+                  <div className="date-content">
+                    <div className="date-outer">
+                      <span className="date">
+                        <span className="year">2020</span>
+                        <span className="cgpa">CGPA 3.75</span>
+                      </span>
                     </div>
                   </div>
-                  <hr />
-                  <div className="row">
-                    <h6 className="mb-0">Experience</h6>
-                    <label className="col-sm-3 date-label text-secondary">
-                      May 2022 - Current
-                    </label>
-                    <div className="col-sm-9">Intern-Net Ltd.</div>
-                    <div className="col-sm-3"></div>
-                    <div className="col-sm-9 text-secondary">
-                      Software Engineer Lead
-                    </div>
-                  </div>
-                  <hr />
-                  <div className="row">
-                    <h6 className="mb-0">Additional Information</h6>
-                    <label className="col-sm-3 date-label text-secondary">
-                      Expected Salary
-                    </label>
-                    <div className="col-sm-9 ">MYR 8,000</div>
-                    <label className="col-sm-3 date-label text-secondary">
-                      Preffered Location
-                    </label>
-                    <div className="col-sm-9 ">
-                      Selangor, Kuala Lumpur, Johor
-                    </div>
-                  </div>
-                  <hr />
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">About Me</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary"></div>
-                    <label className="col-sm-3 date-label text-secondary">
-                      Address
-                    </label>
-                    <div className="col-sm-9 ">
-                      {user.location}
-                    </div>
-                    <label className="col-sm-3 date-label text-secondary">
-                      Nationality
-                    </label>
-                    <div className="col-sm-9 ">Malaysia</div>
+                  <div className="timeline-content">
+                    <h5 className="title">University Tunku Abdul Rahman</h5>
+                    <p className="description">Diploma in Computer Science</p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+          <hr />
+          <h1>Skills</h1>
+          <div className="row profile_main">
+            <div className="col-sm-4">
+              <div className="card card-rounded animated animatedFadeInUp fadeInUp lazy">
+                <p>HTML</p>
+                <div
+                  style={{
+                    width: "100%",
+                    height: "40px",
+                    position: "absolute",
+                    top: "60%",
+                    left: 0,
+                    marginTop: "-20px",
+                    lineHeight: "19px",
+                    textAlign: "center",
+                    fontSize: "28px",
+                    fontWeight: "bold",
+                  }}
+                >
+                  9/10
+                </div>
+                <Doughnut
+                  options={{ tooltips: { enabled: false } }}
+                  data={data}
+                />
+              </div>
+            </div>
+            <div className="col-sm-4">
+              <div className="card card-rounded animated animatedFadeInUp fadeInUp lazy">
+                <p>Javascript</p>
+                <div
+                  style={{
+                    width: "100%",
+                    height: "40px",
+                    position: "absolute",
+                    top: "60%",
+                    left: 0,
+                    marginTop: "-20px",
+                    lineHeight: "19px",
+                    textAlign: "center",
+                    fontSize: "28px",
+                    fontWeight: "bold",
+                  }}
+                >
+                  6/10
+                </div>
+                <Doughnut
+                  options={{ tooltips: { enabled: false } }}
+                  data={data2}
+                />
+              </div>
+            </div>
+            <div className="col-sm-4">
+              <div className="card card-rounded animated animatedFadeInUp fadeInUp lazy">
+                <p>Python</p>
+                <div
+                  style={{
+                    width: "100%",
+                    height: "40px",
+                    position: "absolute",
+                    top: "60%",
+                    left: 0,
+                    marginTop: "-20px",
+                    lineHeight: "19px",
+                    textAlign: "center",
+                    fontSize: "28px",
+                    fontWeight: "bold",
+                  }}
+                >
+                  7/10
+                </div>
+                <Doughnut
+                  options={{ tooltips: { enabled: false } }}
+                  data={data3}
+                />
+              </div>
+            </div>
+          </div>
+          <pre></pre>
         </div>
       </div>
     </>
